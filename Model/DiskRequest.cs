6019e0983fc47e9a926e1ed8ed2c6adc7d737abc
vadimHey <mr.vadem2017@gmail.com>
2025-10-06 18:53:02 +0300

    Создана модель запросов с параметрами диска

---
+++ Model/DiskRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace HDDSimulator.Model
+{
+    // Модель запроса для работы с диском
+    public class DiskRequest
+    {
+        public int Id { get; set; }
+        public int CylinderNumber { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime? CompletionTime { get; set; }
+        public required string FileName { get; set; } = string.Empty;
+    }
+}
