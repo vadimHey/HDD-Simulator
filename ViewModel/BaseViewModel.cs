f4aeb618481345e544126ff47ea44fed8f881d80
vadimHey <mr.vadem2017@gmail.com>
2025-10-08 23:46:57 +0300

    Создан базовый класс для оповещения об изменении свойств

---
+++ ViewModel/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HDDSimulator.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
