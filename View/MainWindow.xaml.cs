8d3a9364ce769b09861c00d4cb3ae5e0471a36b7
vadimHey <mr.vadem2017@gmail.com>
2025-10-08 23:35:57 +0300

    Создано главное окно для вывода запросов

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using HDDSimulator.ViewModel;
+using System.Windows;
+
+namespace HDDSimulator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Открываем модальное окно настроек
+            var sw = new SettingsWindow();
+            bool? res = sw.ShowDialog();
+
+            if (res == true)
+            {
+                // Получаем VM — из DataContext или из ресурса
+                var vm = this.DataContext as MainViewModel ?? (MainViewModel)FindResource("MainViewModel");
+
+                // Вызываем стартовую процедуру
+                vm.StartSimulation(
+                    sw.CylinderCount,
+                    sw.HeadMoveTime,
+                    sw.NewRequestProbability,
+                    sw.InitialRequests
+                );
+            }
+        }
+    }
+}
