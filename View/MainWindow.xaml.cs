add00e47049440f17a5ad08035f6b542442201bf
vadimHey <mr.vadem2017@gmail.com>
2025-10-11 13:15:06 +0300

    Удалены обработчики событий из кода окна, реализовано взаимодействие через команды

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -10,26 +10,5 @@ namespace HDDSimulator
             InitializeComponent();
             DataContext = new MainViewModel();
         }
-
-        private void StartButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Открываем модальное окно настроек
-            var sw = new SettingsWindow();
-            bool? res = sw.ShowDialog();
-
-            if (res == true)
-            {
-                // Получаем VM — из DataContext или из ресурса
-                var vm = this.DataContext as MainViewModel ?? (MainViewModel)FindResource("MainViewModel");
-
-                // Вызываем стартовую процедуру
-                vm.StartSimulation(
-                    sw.CylinderCount,
-                    sw.HeadMoveTime,
-                    sw.NewRequestProbability,
-                    sw.InitialRequests
-                );
-            }
-        }
     }
 }
